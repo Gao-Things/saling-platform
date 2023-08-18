238a07afbd017827b52039af8f8da0255ee2d0a6
yiyu7699 <yiyu7699@uni.sydney.edu.au>
2023-08-18 23:21:45 +1000

    recaptcha Api

--- src/main/java/com/usyd/capstone/controller/userController.java
+++ src/main/java/com/usyd/capstone/controller/userController.java
@@ -5,12 +5,15 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.usyd.capstone.common.QueryPageParam;
 import com.usyd.capstone.common.Result;
+import com.usyd.capstone.entity.DTO.RecaptchaResponse;
 import com.usyd.capstone.entity.User;
 import com.usyd.capstone.entity.VO.UserLogin;
 import com.usyd.capstone.service.UserService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.List;
@@ -18,6 +21,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/user")
 public class userController {
+
+    @Value("${recaptcha.secretKey}")
+    private String recaptchaSecretKey;
     @GetMapping
     public String hello(){
         return "hello";
@@ -32,6 +38,32 @@ public class userController {
         return userService.verifyLogin(userLogin);
 
     }
+    //The token will from the mobile
+    @PostMapping("/reCAPTCHA")
+    public Result submitForm(@RequestBody String recaptchaResponse) {
+        // Validate reCAPTCHA response in the backend
+        boolean isRecaptchaValid = validateRecaptcha(recaptchaResponse);
+
+        if (isRecaptchaValid) {
+            // Perform further processing or save data
+            return Result.suc("reCAPTCHA verification successes");
+        } else {
+            return Result.fail("reCAPTCHA verification failed");
+        }
+    }
+
+    private boolean validateRecaptcha(String recaptchaResponse) {
+        String url = "https://www.google.com/recaptcha/api/siteverify";
+        String params = "?secret=" + recaptchaSecretKey + "&response=" + recaptchaResponse;
+
+        RestTemplate restTemplate = new RestTemplate();
+        RecaptchaResponse response = restTemplate.getForObject(url + params, RecaptchaResponse.class);
+
+        assert response != null;
+        return response.isSuccess();
+    }
+
+
 
     @GetMapping("/List")
     public List<User> list(){
