d74cb4fadddac5d0f497254a17ce0cd3091d2a8b
Mengting Huang <mhua2244@uni.sydney.edu.au>
2023-08-28 15:05:52 +1000

    fix bug

--- src/main/java/com/usyd/capstone/service/impl/ProductServiceImpl.java
+++ src/main/java/com/usyd/capstone/service/impl/ProductServiceImpl.java
@@ -4,24 +4,15 @@ import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.usyd.capstone.common.DTO.ExchangeRateResponse;
-import com.usyd.capstone.common.DTO.Result;
-import com.usyd.capstone.common.DTO.productDetailWithPrice;
 import com.usyd.capstone.entity.ExchangeRateUsd;
 import com.usyd.capstone.entity.Product;
-import com.usyd.capstone.entity.User;
 import com.usyd.capstone.mapper.ExchangeRateUsdMapper;
 import com.usyd.capstone.mapper.ProductMapper;
 import com.usyd.capstone.service.ProductService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * <p>
