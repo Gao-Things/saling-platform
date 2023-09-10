6f571f8dc387f65a149e027da962ae7d1d6a82e9
Liuying Cui <lcui6955@uni.sydney.edu.au>
2023-09-10 22:07:35 +1000

    public service test

---
+++ src/test/java/com/usyd/capstone/PublicServiceTest.java
@@ -0,0 +1,21 @@
+package com.usyd.capstone.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PublicServiceTest {
+
+    @Test
+    public void verifyLogin() {
+
+    }
+
+    @Test
+    public void registration() {
+    }
+
+    @Test
+    public void registrationVerification() {
+    }
+}
