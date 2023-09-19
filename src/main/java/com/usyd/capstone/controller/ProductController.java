41b3f465ed101ce2572eb8fefda0ba6849a6da29
yiyu7699 <yiyu7699@uni.sydney.edu.au>
2023-09-19 17:49:37 +1000

    upload images

--- src/main/java/com/usyd/capstone/controller/ProductController.java
+++ src/main/java/com/usyd/capstone/controller/ProductController.java
@@ -10,12 +10,12 @@ import com.usyd.capstone.entity.Product;
 import com.usyd.capstone.service.ExchangeRateUsdService;
 import com.usyd.capstone.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
+import java.io.File;
+import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.List;
@@ -66,6 +66,41 @@ public class ProductController {
         resultMap.put("CurrencyUnit", targetCurrency);
         resultMap.put("ProductList", productList);
         return Result.suc(resultMap);
+    }
+    @Value("${upload.dir}") // 从配置文件获取上传目录
+    private String uploadDir;
+
+    @Value("${image.Url}") // 从配置文件获取上传目录
+    private String imageUrl;
+
+    @PostMapping("/uploadImage")
+    public Result uploadImage(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return Result.fail("Please select a file to upload.");
+        }
+
+        try {
+            // 确保上传目录存在，如果不存在则创建
+            File dir = new File(uploadDir);
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+
+            // 生成新的文件名，将时间戳添加到原始文件名中
+            String originalFileName = file.getOriginalFilename();
+            assert originalFileName != null;
+            String fileExtension = originalFileName.substring(originalFileName.lastIndexOf("."));
+            String fileNameWithoutExtension = originalFileName.substring(0, originalFileName.lastIndexOf("."));
+            String newFileName = fileNameWithoutExtension + System.currentTimeMillis() + fileExtension;
+            String filePath = uploadDir + File.separator + newFileName;
+            File dest = new File(filePath);
+            file.transferTo(dest);
+            String imageUrlUse = imageUrl + newFileName;
+            // 返回成功响应
+            return Result.suc(imageUrlUse);
+        }  catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
